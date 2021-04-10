9d81e679eec07b6d4954f9340919fec2724e93ec
Emily Eisenberg <xymostech@gmail.com>
2021-04-10 14:41:39 -0700

    Initial source commit

---
+++ static/script.js
@@ -0,0 +1,37 @@
+let maps = [["Academy Map","4","71","Æ"],["Acid Caverns Map","14","81","ÿ"],["Acton's Nightmare","9","76","Ô"],["Alleyways Map","8","75","a"],["Altered Distant Memory","4","71","u"],["Ancient City Map","3","70","R"],["Arachnid Nest Map","16","83","Ó"],["Arachnid Tomb Map","15","82","!"],["Arcade Map","1","68","+"],["Arena Map","2","69","t"],["Arid Lake Map","3","70","c"],["Armoury Map","5","72","r"],["Arsenal Map","4","71","é"],["Ashen Wood Map","3","70","K"],["Atoll Map","5","72","Í"],["Augmented Distant Memory","4","71","f"],["Barrows Map","14","81","n"],["Basilica Map","3","70","»"],["Bazaar Map","15","82","Y"],["Beach Map","14","81","X"],["Belfry Map","3","70","w"],["Bog Map","15","82","y"],["Bone Crypt Map","4","71","V"],["Bramble Valley Map","14","81","™"],["Bramble Valley Map","14","81","™"],["Burial Chambers Map","16","83","E"],["Caer Blaidd, Wolfpack's Den","2","69","æ"],["Cage Map","4","71","I"],["Caldera Map","14","81","ø"],["Canyon Map","3","70","j"],["Carcass Map","5","72","§"],["Castle Ruins Map","15","82","ê"],["Cells Map","5","72","Ú"],["Cemetery Map","3","70","L"],["Channel Map","4","71","Z"],["Chateau Map","5","72","O"],["City Square Map","14","81","Ö"],["Cold River Map","11","78","ë"],["Colonnade Map","15","82","."],["Colosseum Map","14","81","ü"],["Conservatory Map","11","78","6"],["Coral Ruins Map","12","79","Á"],["Core Map","14","81","û"],["Cortex","9","76","1"],["Courthouse Map","4","71","f"],["Courtyard Map","3","70","4"],["Coves Map","1","68","Ê"],["Crater Map","2","69","D"],["Crimson Temple Map","4","71","©"],["Crimson Township Map","12","79","%"],["Crystal Ore Map","4","71","å"],["Cursed Crypt Map","13","80","}"],["Dark Forest Map","7","74","®"],["Death and Taxes","16","83","¿"],["Defiled Cathedral Map","2","69","ú"],["Desert Map","4","71","W"],["Desert Spring Map","5","72","ç"],["Dig Map","12","79","†"],["Doryani's Machinarium","13","80","‹"],["Dry Sea Map","8","75","`"],["Dunes Map","4","71","A"],["Dungeon Map","9","76","3"],["Estuary Map","15","82","s"],["Excavation Map","14","81","S"],["Factory Map","3","70","Î"],["Fields Map","2","69","Ñ"],["Flooded Mine Map","11","78","?"],["Forbidden Woods Map","10","77","'"],["Forge of the Phoenix Map","16","83","¤"],["Forking River Map","3","70","^"],["Foundry Map","15","82","à"],["Frozen Cabins Map","2","69","ä"],["Frozen Cabins Map","2","69","ä"],["Fungal Hollow Map","11","78","d"],["Gardens Map","2","69","J"],["Geode Map","3","70","\""],["Ghetto Map","10","77","œ"],["Glacier Map","3","70","U"],["Grave Trough Map","11","78","$"],["Grave Trough Map","11","78","$"],["Graveyard Map","3","70","N"],["Grotto Map","2","69","h"],["Hall of Grandmasters","2","69","Ù"],["Hallowed Ground","12","79","Ð"],["Haunted Mansion Map","5","72","k"],["Iceberg Map","4","71","H"],["Infested Valley Map","2","69","Œ"],["Ivory Temple Map","11","78","m"],["Jungle Valley Map","1","68","5"],["Laboratory Map","3","70","Ä"],["Lair Map","2","69",","],["Lair of the Hydra Map","16","83","¢"],["Lava Chamber Map","3","70","Q"],["Lava Lake Map","15","82","µ"],["Leyline Map","4","71","G"],["Lighthouse Map","5","72","F"],["Lookout Map","4","71","C"],["Maelström of Chaos","6","73","Ç"],["Malformation Map","6","73","ñ"],["Mao Kun","9","76","À"],["Marshes Map","4","71","2"],["Mausoleum Map","9","76","["],["Maze Map","13","80","l"],["Maze of the Minotaur Map","16","83","£"],["Mesa Map","14","81","Ò"],["Mineral Pools Map","9","76","{"],["Moon Temple Map","2","69","="],["Mud Geyser Map","3","70","É"],["Museum Map","4","71","("],["Necropolis Map","16","83","°"],["Oba's Cursed Trove","9","76","q"],["Olmec's Sanctum","8","75","$"],["Orchard Map","3","70","<"],["Overgrown Ruin Map","3","70","]"],["Overgrown Shrine Map","9","76","¾"],["Palace Map","5","72","\\"],["Park Map","13","80","÷"],["Pen Map","1","68","b"],["Peninsula Map","2","69","*"],["Phantasmagoria Map","10","77","o"],["Pier Map","3","70","z"],["Pillars of Arun","8","75","›"],["Pit Map","16","83","×"],["Pit of the Chimera Map","16","83","Â"],["Plaza Map","3","70","€"],["Poorjoy's Asylum","10","77","ß"],["Port Map","11","78","e"],["Precinct Map","4","71",":"],["Primordial Blocks Map","3","70","â"],["Primordial Pool Map","4","71","Â"],["Promenade Map","2","69",";"],["Racecourse Map","8","75","«"],["Ramparts Map","4","71","Ü"],["Reef Map","3","70","7"],["Relic Chambers Map","9","76","1"],["Replica Cortex","9","76","1"],["Replica Pillars of Arun","8","75","›"],["Replica Poorjoy's Asylum","10","77","ß"],["Residence Map","14","81","T"],["Rewritten Distant Memory","5","72","»"],["Scriptorium Map","10","77","‡"],["Sepulchre Map","14","81","Ÿ"],["Shipyard Map","12","79","v"],["Shore Map","3","70","/"],["Shrine Map","5","72","è"],["Siege Map","12","79","u"],["Silo Map","6","73","•"],["Silo Map","6","73","•"],["Spider Forest Map","2","69","Ã"],["Spider Lair Map","9","76","M"],["Stagnation Map","9","76","¢"],["Stagnation Map","9","76","¢"],["Strand Map","3","70","8"],["Sulphur Vents Map","3","70","P"],["Summit Map","15","82","ù"],["Sunken City Map","2","69","•"],["Temple Map","3","70","-"],["Terrace Map","15","82","ð"],["The Coward's Trial","13","80","Å"],["The Perandus Manor","8","75","™"],["The Putrid Cloister","11","78","Õ"],["The Shaper's Realm","17","84","¥"],["The Twilight Temple","2","69","^"],["The Vinktar Square","3","70","²"],["Thicket Map","14","81","B"],["Tower Map","2","69","0"],["Toxic Sewer Map","3","70","Ï"],["Tropical Island Map","2","69","Û"],["Twisted Distant Memory","4","71","÷"],["Underground River Map","2","69","9"],["Underground Sea Map","2","69","q"],["Untainted Paradise","5","72","Û"],["Vaal Pyramid Map","3","70","~"],["Vaal Temple Map","16","83",">"],["Vault Map","15","82",")"],["Vaults of Atziri","12","79","g"],["Villa Map","15","82","³"],["Volcano Map","2","69","i"],["Waste Pool Map","5","72","_"],["Wasteland Map","4","71","x"],["Waterways Map","7","74","Ì"],["Whakawairua Tuahu","4","71","È"],["Wharf Map","2","69","Ë"]];
+
+maps.sort((a, b) => a[3].localeCompare(b[3]));
+
+maps = maps.filter((map, i) => i === 0 || maps[i - 1][0] !== map[0]);
+
+const chars = maps.filter((map, i) => i === 0 || maps[i - 1][3] !== map[3]).map(map => map[3]);
+
+const output = document.querySelector(".outputs");
+
+for (const select of document.querySelectorAll("select")) {
+  const option = document.createElement("option");
+  option.setAttribute("value", "");
+  option.innerText = "";
+  select.appendChild(option);
+
+  for (const char of chars) {
+    const option = document.createElement("option");
+    option.setAttribute("value", char);
+    option.innerText = char;
+    select.appendChild(option);
+  }
+
+  const out = document.createElement("div");
+  out.setAttribute("class", "output");
+  output.appendChild(out);
+  out.innerText = "—";
+
+  select.addEventListener("change", function(e) {
+    if (e.target.value === "") {
+      out.innerText = "—";
+      return;
+    }
+    const matchingMaps = maps.filter(map => map[3] === e.target.value);
+    out.innerText = matchingMaps.map(map => map[0] + " (Tier " + map[1] + ")").join(', ');
+  });
+}
